05bb854c3fa4bf0dc5f98f47f90d3e4e80656de3
Thutmose <elpatricimo@gmail.com>
2019-06-06 18:18:33 -0400

    Adds lang files

--- src/main/java/pokecube/legends/Reference.java
+++ src/main/java/pokecube/legends/Reference.java
@@ -2,7 +2,7 @@ package pokecube.legends;
 
 public class Reference
 {
-    public static final String ID          = "pokecube-legends";
+    public static final String ID          = "pokecube_legends";
     public static final String VERSION     = "@VERSION@";
     public static final String NAME        = "Pokecube Legends";
     public static final String DEPSTRING   = "required-after:pokecube";
