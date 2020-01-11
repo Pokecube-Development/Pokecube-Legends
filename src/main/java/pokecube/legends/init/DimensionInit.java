a6ce7642563bfb9a20de4d5889e970d58483c3fa
Thutmose <elpatricimo@gmail.com>
2020-01-11 13:15:41 -0500

    Update DimensionInit.java
    re-organizes the dimension init, might help with dimension id conflicts

--- src/main/java/pokecube/legends/init/DimensionInit.java
+++ src/main/java/pokecube/legends/init/DimensionInit.java
@@ -6,32 +6,55 @@ import net.minecraft.world.DimensionType;
 import net.minecraftforge.common.DimensionManager;
 import pokecube.legends.worldgen.DimensionTypeUltraSpace;
 
-public class DimensionInit {
-	
-	public static void MainRegistry() {
-		registerDimension();
-	}
-	
-	public static final int ultraspaceDimensionID = findFreeDimensionID();
-	public static final DimensionType ULTRASPACE = DimensionType.register("Ultra_Space", "_ultraspace", ultraspaceDimensionID, DimensionTypeUltraSpace.class, false);
-	
-	public static void registerDimension() {
-		DimensionManager.registerDimension(ultraspaceDimensionID, ULTRASPACE);
-	}
-	
-	@Nullable
+public class DimensionInit
+{
+    public static int           ultraspaceDimensionID;
+    public static int           ultraspacDimensionTypeID;
+    public static DimensionType ULTRASPACE;
+
+    public static void registerDimension()
+    {
+        ultraspaceDimensionID = findFreeDimensionID();
+        ultraspacDimensionTypeID = findFreeDimensionTypeID();
+        ULTRASPACE = DimensionType.register("Ultra_Space", "_ultraspace", ultraspacDimensionTypeID,
+                DimensionTypeUltraSpace.class, false);
+        DimensionManager.registerDimension(ultraspaceDimensionID, ULTRASPACE);
+    }
+
+    @Nullable
+    private static Integer findFreeDimensionTypeID()
+    {
+        for (int i = 2; i < Integer.MAX_VALUE; i++)
+        {
+            try
+            {
+                DimensionType.getById(i);
+            }
+            catch (Exception e)
+            {
+                // Unused dimension type!
+                return i;
+            }
+        }
+
+        // DEBUG
+        System.out.println("ERROR: Could not find free dimension ID");
+        return null;
+    }
+
+    @Nullable
     private static Integer findFreeDimensionID()
     {
-        for (int i=2; i<Integer.MAX_VALUE; i++)
+        for (int i = 2; i < Integer.MAX_VALUE; i++)
         {
             if (!DimensionManager.isDimensionRegistered(i))
             {
                 // DEBUG
-                System.out.println("Found free dimension ID = "+i);
+                System.out.println("Found free dimension ID = " + i);
                 return i;
             }
         }
-        
+
         // DEBUG
         System.out.println("ERROR: Could not find free dimension ID");
         return null;
