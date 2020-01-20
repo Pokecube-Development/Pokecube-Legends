b3e81c79b4662d774c21ed6e6bb270d9a5b6295f
Thutmose <elpatricimo@gmail.com>
2020-01-20 14:10:02 -0500

    Update PortalSpawnHandler.java
    adjusts spawning of mirage spots to prevent them in the nether and end, also prevents them spawning over void.
    
    They are specifically prevented in the end, but for other dimension mods, they might work in the other dimensions, but shouldn't spawn over the void anymore.
    
    This addresses https://github.com/Pokecube-Development/Pokecube-Issues-and-Wiki/issues/152

--- src/main/java/pokecube/legends/handlers/PortalSpawnHandler.java
+++ src/main/java/pokecube/legends/handlers/PortalSpawnHandler.java
@@ -12,6 +12,8 @@ import net.minecraftforge.fml.common.gameevent.TickEvent.WorldTickEvent;
 import net.minecraftforge.fml.relauncher.Side;
 import pokecube.core.PokecubeCore;
 import pokecube.core.database.Database;
+import pokecube.core.events.handlers.SpawnHandler;
+import pokecube.core.events.handlers.SpawnHandler.ForbidReason;
 import pokecube.legends.PokecubeLegends;
 import pokecube.legends.init.BlockInit;
 import thut.api.maths.Vector3;
@@ -30,6 +32,9 @@ public class PortalSpawnHandler
 
     public void tick(World world)
     {
+        // Only do this on surface worlds.
+        if (!world.provider.isSurfaceWorld()) return;
+
         List<Object> players = new ArrayList<Object>(world.playerEntities);
         if (players.size() < 1) return;
         Random rand = new Random();
@@ -41,6 +46,11 @@ public class PortalSpawnHandler
         v.x += dx;
         v.z += dz;
         v.y = world.getHeight((int) v.x, (int) v.z) + 3;
+        // No spawning on void.
+        if (v.y < 4) return;
+        // No spawning near repels.
+        if (SpawnHandler.getNoSpawnReason(world, v.intX(), v.intY(), v.intZ()) == ForbidReason.REPEL) return;
+        // Only spawn if air.
         if (v.isAir(world)) world.setBlockState(v.getPos(), BlockInit.BLOCK_PORTALWARP.getDefaultState());
 
     }
